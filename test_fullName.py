8f6b9a4eafb691e1d306e117beae9d7e144d3c36
JetLiTheQT <ekkachai.ittihrit2@gmail.com>
2021-05-03 21:44:32 -0700

    Added Fail Tests

--- test_fullName.py
+++ test_fullName.py
@@ -3,10 +3,19 @@ import fullName
 
 class testFullName(unittest.TestCase):
     def test_apostropheNames(self):
+        #Passing Test
         self.assertEqual(fullName.fullName("D'Amato", "Lexington"), "D'Amato Lexington")
+        #Failing Test
+        #self.assertEqual(fullName.fullName("D'Amato", "Lexington"), "D'AmatoLexington")
     def test_randomStrings(self):
+        #Passing Test
         self.assertEqual(fullName.fullName("123,&Hane", "Po'9zna\]"), "123,&Hane Po'9zna\]" )
+        #Failing Test
+        #self.assertEqual(fullName.fullName("123,&Hane", "Po'9zna\]"), "123,&Hane Po'9zna\" )
     def test_noInput(self):
+        #Passing Test
         self.assertEqual(fullName.fullName("", ""), " ")
+        #Failing Test
+        #self.assertEqual(fullName.fullName("", ""), "")
 if __name__ == '__main__':
     unittest.main()
